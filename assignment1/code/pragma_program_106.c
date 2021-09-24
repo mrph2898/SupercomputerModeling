cf0275b2646adcdd996b868e38e68ecea9b98664
mrph2898 <egor2898@mail.ru>
2021-09-24 11:34:49 +0300

    Change wrong pragma position

--- assignment1/code/pragma_program_106.c
+++ assignment1/code/pragma_program_106.c
@@ -5,9 +5,9 @@ for(i = 2; i <= n+1; ++i)
     for(j = 2; j <= m+1; ++j)
         B[i][j] = B[i + 1][j - 1];
 
+#pragma omp parallel for
 for(i = 2; i <= n+1; ++i){
     A[i][1][1] = B[i][m + 1] + C[n + 1];
-#pragma omp parallel for
     for(j = 2; j <= m+1; ++j)
 #pragma omp parallel for
         for(k = 1; k <= n; ++k)
